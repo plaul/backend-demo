220ae48973ef9fd6f23678a49815ec9840046f1b
plaul <lmor@kea.dk>
2021-09-01 15:34:15 +0200

    initial commit

---
+++ src/main/java/sem3/backend/ongoing/OngoingApplication.java
@@ -0,0 +1,13 @@
+package sem3.backend.ongoing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OngoingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OngoingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sem3/backend/ongoing/OngoingApplicationTests.java
@@ -0,0 +1,13 @@
+package sem3.backend.ongoing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OngoingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
